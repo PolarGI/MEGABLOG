c195a06be91c1f561ee3cab2330ba92f7195fff6
PolarGI <102267762+PolarGI@users.noreply.github.com>
2022-03-31 20:33:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function spanelem() {
+    console.log("Este es un elemento SPAN");
+  }
+  
+  function divelem() {
+    console.log("Este es un elemento DIV");
+    document.getElementById("audio").play();
+  }
+  
+  
